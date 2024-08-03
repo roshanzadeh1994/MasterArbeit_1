c0274b69a43982a0da39f6bc2cfd5d9196617a4d
roshanzadeh1994 <86236945+roshanzadeh1994@users.noreply.github.com>
2024-08-03 13:14:13 +0200

    alles mit router2 funktioniert richtig

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException, Request
 from db.database import Base, get_db
 from db.database import engine
-from routers import  user_router, router, router_ai
+from routers import  user_router, router, router_ai,router_ai2
 from auth import authentication
 from fastapi.staticfiles import StaticFiles
 
@@ -15,7 +15,7 @@ app.mount("/static", StaticFiles(directory="templates"), name="static")
 app.include_router(user_router.router)
 app.include_router(router.router)
 app.include_router(authentication.router)
-app.include_router(router_ai.router)
+app.include_router(router_ai2.router)
 
 Base.metadata.create_all(bind=engine)
 
